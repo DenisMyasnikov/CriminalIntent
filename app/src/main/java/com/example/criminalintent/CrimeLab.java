646199d2ad833e43491e9e1f3a59d3b1b752aa97
Денис <denismyasnikov@bk.ru>
2020-01-19 23:41:06 +0300

    Добавлены классф для будущей реализации списка

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -2,6 +2,7 @@ package com.example.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,7 +17,15 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
-    private CrimeLab(Context context){}
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++){
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" +  i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
 
     public List<Crime> getCrimes() {
         return mCrimes;
